e6882e69c00106584e0d31719c2967e29a327601
Naoshinaps <n.paillallao.17@gmail.com>
2026-07-04 16:56:43 -0400

    se modifico appConfig

--- src/main/java/com/HomeRentSolution/MS_Pagos/config/AppConfig.java
+++ src/main/java/com/HomeRentSolution/MS_Pagos/config/AppConfig.java
@@ -10,21 +10,24 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     @Bean
     public Request.Options feignOptions() {
-
         return new Request.Options(5000, 10000);
     }
 
     public static final String PAGOS_EXCHANGE = "pagos.exchange";
+    public static final String RESERVAS_EXCHANGE = "reservas.exchange"; // NUEVO: exchange del publicador
     public static final String RESERVAS_QUEUE = "pagos.reserva-creada.queue";
     public static final String QUEUE_RESERVA_CANCELADA = "pagos.reserva-cancelada.queue";
-
-
     public static final String ROUTING_CREADO = "pago.creado";
     public static final String ROUTING_ELIMINADO = "pago.eliminado";
 
     @Bean
     public TopicExchange pagosExchange() {
         return new TopicExchange(PAGOS_EXCHANGE);
+    }
+
+    @Bean
+    public TopicExchange reservasExchange() { // declaramos el exchange de Reserva para poder enlazarnos
+        return new TopicExchange(RESERVAS_EXCHANGE);
     }
 
     @Bean
@@ -38,13 +41,17 @@ public class AppConfig {
     }
 
     @Bean
-    public Binding bindingReservas(Queue reservasQueue, TopicExchange pagosExchange) {
-        return BindingBuilder.bind(reservasQueue).to(pagosExchange).with("pago.*");
+    public Binding bindingReservaCreada(Queue reservasQueue, TopicExchange reservasExchange) {
+        return BindingBuilder.bind(reservasQueue).to(reservasExchange).with("reserva.creada");
+    }
+
+    @Bean
+    public Binding bindingReservaCancelada(Queue reservaCanceladaQueue, TopicExchange reservasExchange) {
+        return BindingBuilder.bind(reservaCanceladaQueue).to(reservasExchange).with("reserva.cancelada");
     }
 
     @Bean
     public Jackson2JsonMessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
     }
-
 }
